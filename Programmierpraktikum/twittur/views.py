a1a5da712c3b77af6f68ffcfc03970b9489f1c3e
Willy Cai <willy.cai@icloud.com>
2015-05-29 00:46:36 +0200

    Login / Registrierung Update
    - Fehlermeldung integriert:
    
    - *NEUES FELD* Passwort erneut eingeben
    - gibt nun Fehler aus, falls Passwörter nicht übereinstimmen
    - gibt nun Fehler aus, falls Username schon vergeben ist
    …
    - zusammengefasst: gibt Fehler aus falls was nicht stimmt bei Login und
    Registrierung!!!
    
    - Logout Button eingefügt (NOT FINAL)
    - User kann sich nun ausloggen
    - eingeloggte User können nicht mehr auf die Login/Registrierungsseite
    gehen

--- Programmierpraktikum/twittur/views.py
+++ Programmierpraktikum/twittur/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from Programmierpraktikum.settings import MEDIA_ROOT, MEDIA_URL
 from django.contrib.auth.models import User
 from django.db.models import Q
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.contrib.auth import authenticate, login
 from django.contrib import auth
@@ -14,19 +17,20 @@ from .models import UserProfile, Group, Nav, Message
 # startpage
 def index(request):
 
+
 	# redirect, if user is not authenticated 
 	if not request.user.is_authenticated():
 		return HttpResponseRedirect('/twittur/login/')
 
-	print(request.user)
+	#print(request.user)
 	current_user = User.objects.all().filter(username__exact=request.user.username).select_related('userprofile')
 	user_list = UserProfile.objects.all().filter(userprofile__exact=request.user)
 	atTag = '@' + request.user.username + ' '
-	print (atTag)
+	#print (atTag)
 	message_list = Message.objects.all().select_related('user__userprofile').filter( Q(user__exact=request.user) | Q(text__contains=atTag))
-	print(message_list)
+	#print(message_list)
 	messageList = Message.objects.all()
-	print(messageList)
+	#print(messageList)
 	#user_list = User.objects.all()
 	#message_list = Message.objects.all()
 	#message_list = Message.objects.all().select_related('user')
@@ -39,44 +43,74 @@ def index(request):
 # login/registration page
 def login(request):
 
+	if request.user.is_authenticated():
+		return HttpResponseRedirect('/twittur/')
+
 	# login
 	if request.method == "GET":
-		query_dict = request.GET
-		username = query_dict.get('username')
-		password = query_dict.get('password')
-		user = authenticate(username=username, password=password)
-		context = { 'active_page' : 'index', 'nav': Nav.nav }
+		if 'login' in request.GET:
+			query_dict = request.GET
+			username = query_dict.get('username')
+			password = query_dict.get('password')
+			user = authenticate(username=username, password=password)
+			context = { 'active_page' : 'index', 'nav': Nav.nav }
 
-		if user is not None:
-			if user.is_active:
-				auth.login(request, user)
-				return HttpResponseRedirect('/twittur/')
-		else:
-			return render(request, 'ftu.html')			
+			if user is not None:
+				if user.is_active:
+					auth.login(request, user)
+					return HttpResponseRedirect('/twittur/')
+			elif username=="" and password=="" :
+				error_text = "Geben sie bitte Username und Passwort ein."
+				return render(request, 'ftu.html', { 'error_text' : error_text } )	
+			else:
+				error_text = "Ups, Username oder Passwort falsch."
+				active_toggle = "active_toggle"
+				return render(request, 'ftu.html', { 'error_text' : error_text } )		
 
 	# Registration
 	if request.method == 'POST':
+
 		query_dict = request.POST
+		print(query_dict)
 		username = query_dict.get('name')
 
 		# check if available
-		checkUsername = User.objects.get( username__exact = username )
-		if checkUsername.username == username:
-			return render(request, 'ftu.html')
+		try:
 
-		password = query_dict.get('password')
-		email = query_dict.get('email')
-		user = User.objects.create_user( username, email, password )
-		user_profil = UserProfile(user=user, studentNumber=000000, academicDiscipline='Informatik')
-		user_profil.save()
-		user = authenticate(username = username, password = password)
-		context = { 'active_page' : 'index', 'nav': Nav.nav , 'user' : user_profil }
-		auth.login(request, user)
-		return render(request, 'index.html', context)
+			checkUsername = User.objects.get( username__exact = username )
+			print(checkUsername)
+		except ObjectDoesNotExist:
+			password = query_dict.get('password')
+			ack_password = query_dict.get('ack_password')
+
+			if password != ack_password :
+				error_password = "Passwörter sind nicht gleich."
+				return render(request, 'ftu.html', { 'error_password': error_password })
+
+			email = query_dict.get('email')
+			user = User.objects.create_user( username, email, password )
+			user_profil = UserProfile(userprofile=user, studentNumber=000000, academicDiscipline='Informatik')
+			user_profil.save()
+			user = authenticate(username = username, password = password)
+			context = { 'active_page' : 'index', 'nav': Nav.nav , 'user' : user_profil }
+			auth.login(request, user)
+			
+			return render(request, 'index.html', context)
+		else:
+	
+			error_user = "Sorry, Username ist vergeben."
+			return render(request, 'ftu.html', { 'error_user': error_user })
 
 	# twitter/login/
 	context = { 'active_page' : 'ftu', 'nav': Nav.nav}
 	return render(request, 'ftu.html', context)
+# logout
+def logout(request):
+	
+	auth.logout(request)
+	
+	#return render(request, 'ftu.html')
+	return HttpResponseRedirect('/twittur/')
 
 # profilpage
 def profile(request):
