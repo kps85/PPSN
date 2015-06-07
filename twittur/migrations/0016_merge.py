86506b1d1646ba687298e53e3118084b8a9a355e
Willy <willy.cai@icloud.com>
2015-06-07 18:09:59 +0200

    database migration
    - was missing

---
+++ twittur/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('twittur', '0014_auto_20150606_0850'),
+        ('twittur', '0015_auto_20150607_0028'),
+    ]
+
+    operations = [
+    ]
